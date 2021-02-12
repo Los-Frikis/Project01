5609fb6717aee3cff1c36bcedacfb2ae4531f22c
Simon Alvarez <78326030+Simonalgo@users.noreply.github.com>
2021-02-12 15:51:00 -0500

    Version 1.0.0

--- Robot.java
+++ Robot.java
@@ -8,12 +8,57 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Robot extends Actor
 {
-    /**
+  /**
      * Act - do whatever the Robot wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        // Add your action code here.
-    }    
+      comer();
+      morirpared();
+      morirbloque();
+      completado();
+        int y=getY();
+      
+      if(Greenfoot.isKeyDown("right"))
+      move(2);
+      if(Greenfoot.isKeyDown("left"))
+      move(-2); 
+        if(Greenfoot.isKeyDown("up"))
+           y--;
+           if(Greenfoot.isKeyDown("down"))
+           y++; 
+        setLocation(getX(),y);
+    }
+  public void comer(){
+        Actor Apple;
+        Apple = getOneObjectAtOffset(0,0, Apple.class);
+        if (Apple != null){
+            World mundo;
+            mundo = getWorld();
+            mundo.removeObject(Apple);
+            Greenfoot.playSound("eat.wav");
+        }
+    }
+  public void morirpared(){
+        if (isTouching(Wall.class)){
+            setLocation(48,50);
+            Greenfoot.playSound("hurt.wav");
+        }
+    }
+  public void morirbloque(){
+        if (isTouching(Block.class)){
+            setLocation(48,50);
+            Greenfoot.playSound("hurt.wav");
+        }
+    }
+  public void completado(){
+        if (isTouching(Home.class)){
+            setLocation(48,50);
+            Greenfoot.playSound("missionpassed.wav");
+        }
+    }
 }
+
+
+
